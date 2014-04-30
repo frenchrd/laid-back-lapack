fab3c7f13c2e7d8aeb2f67addb96d103f139b57a
Robert D. French <frenchrd@ornl.gov>
2014-04-29 20:26:13 -0400

    Basic Trig Sanity Check

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "math.h"
+#include "lb_vector.h"
 
+int main(int argc, char** agrv) {
+	unsigned int length = atoi(argv[1]);
+	Vector sine_squared = lb_allocate_vector(1024 * 1024 * length);
+	lbv_iterate(sine_squared, i, sine_squared.data[i] = pow(sine(2 * 3.1415 / length * i),2));
 
+	Vector cosine_squared = lb_allocate_vector(1024 * 1024 * length);
+	lbv_iterate(cosine_squared, i, cosine_squared.data[i] = pow(cosine(2 * 3.1415 / length * i),2));
+
+	Scalar one;
+	lbdp(sine_squared,cosine_squared,&one);
+	printf("Sin^2 + Cos^2 = %f\n",one);
+	return 0;
+}
