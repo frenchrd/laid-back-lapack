051cafdd8c6e891e94a334a612bedef85d394663
Robert D. French <frenchrd@ornl.gov>
2014-04-29 21:03:17 -0400

    Use correct pointer notation

--- lb_vector.c
+++ lb_vector.c
@@ -8,15 +8,15 @@ Vector lb_create_vector(Scalar* data, unsigned int length) {
 }
 
 Vector lb_allocate_vector(unsigned int length) {
-	void* ptr = malloc(sizeof(Scalar) * length);
-	return lb_create_vector((Scalar*) ptr, length);
+	Scalar* ptr = (Scalar*)malloc(sizeof(Scalar) * length);
+	return lb_create_vector(ptr, length);
 }
 
 // Result = Vector dot Vector
 void lbdp(Vector a, Vector b, Scalar* result) {
-	&result = 0.0;
+	*result = 0.0;
 	int i;
-	for(i = 0; i < a.length; i++) &result += a.data[i] * b.data[i];
+	for(i = 0; i < a.length; i++) *result += a.data[i] * b.data[i];
 }
 
 // Result = Scalar * Vector
