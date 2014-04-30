c95ccac35a430c53545e10105c56d5ef37655e4f
Robert D. French <frenchrd@ornl.gov>
2014-04-29 21:48:28 -0400

    Remember to define h

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int main(int argc, char** argv) {
 	unsigned int length = atoi(argv[1]);
 
+	Scalar h = (Scalar)(2.0 * 3.1415 / (Scalar)length);
 	Vector sin_squared = lb_allocate_vector(1024l * length);
 	printf("About to create sin_squared\n");
 	lbv_iterate(sin_squared, i, sin_squared.data[i] = (Scalar)pow(sin(h * i),2));
