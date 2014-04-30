d130ce3282bcb9d1681cf109b8e3caba5fd5c4f3
Robert D. French <frenchrd@ornl.gov>
2014-04-29 21:34:27 -0400

    Allocate less memory

--- main.c
+++ main.c
@@ -5,13 +5,17 @@
 
 int main(int argc, char** argv) {
 	unsigned int length = atoi(argv[1]);
-	Vector sin_squared = lb_allocate_vector(1024 * 1024 * length);
-	lbv_iterate(sin_squared, i, sin_squared.data[i] = pow(sin(2 * 3.1415 / length * i),2));
 
-	Vector cos_squared = lb_allocate_vector(1024 * 1024 * length);
-	lbv_iterate(cos_squared, i, cos_squared.data[i] = pow(cos(2 * 3.1415 / length * i),2));
+	Vector sin_squared = lb_allocate_vector(1024l * length);
+	printf("About to create sin_squared\n");
+	lbv_iterate(sin_squared, i, sin_squared.data[i] = (Scalar)pow(sin(h * i),2));
+
+	Vector cos_squared = lb_allocate_vector(1024l * length);
+	printf("About to create cos_squared\n");
+	lbv_iterate(cos_squared, i, cos_squared.data[i] = (Scalar)pow(cos(h * i),2));
 
 	Scalar one;
+	printf("About to create dot product\n");
 	lbdp(sin_squared,cos_squared,&one);
 	printf("Sin^2 + Cos^2 = %f\n",one);
 	return 0;
