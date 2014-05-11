23d3def3fbf02ddace0097c4bdb26f5a8617e213
Robert D. French <frenchrd@ornl.gov>
2014-05-11 14:52:27 -0400

    Implement matrix multiplation correctly

--- lb_matrix.c
+++ lb_matrix.c
@@ -1,18 +1,23 @@
 #include "lb_matrix.h"
+#include <stdlib.h>
 // Assume A.num_cols == B.num_rows
-// Assume A.num_rows == B.num_cols
+// 	  C.num_rows == A.num_rows
+// 	  C.num_cols == B.num_cols
 void lbmm(Matrix A, Matrix B, Matrix result) {
 	int col,row,offset,vec_length;
-	double temp;
+	double vec_sum;
 	vec_length = A.num_cols;
-	for(int col = 0; col < A.num_cols; col++) {
+	for(col = 0; col < B.num_cols; col++) {
 		for(row = 0; row < A.num_rows; row++) {
-			// Dot Product Row_i(A) with Col_j(B)
-			temp = 0.0;
+			// Dot Product Row(A) with Col(B)
+			vec_sum = 0.0;
 			for(offset = 0; offset < vec_length; offset++) {
-				temp += lb_mat_row_element(A,row,offset) * lb_mat_col_element(B,col,offset);
+				double a = lb_mat_row_element(A,row,offset);
+				double b = lb_mat_col_element(B,col,offset);
+				double product = a * b;
+				vec_sum += product;
 			}
-			lb_mat_element(result,row,col) = temp;
+			lb_mat_element(result,row,col) = vec_sum;
 		}
 	}
 }
@@ -22,7 +27,7 @@ void lbmm(Matrix A, Matrix B, Matrix result) {
 // Assume A.num_rows == B.num_rows
 void lbma(Matrix A, Matrix B, Matrix result) {
 	int col,row;
-	for(int col = 0; col < A.num_cols; col++) {
+	for(col = 0; col < A.num_cols; col++) {
 		for(row = 0; row < A.num_rows; row++) {
 			lb_mat_element(result,row,col) = lb_mat_element(A,row,col) + lb_mat_element(B,row,col);
 		}
