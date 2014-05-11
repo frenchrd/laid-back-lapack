3236ff1f1c85e35d99aba3c94092a665717964ef
Robert D. French <frenchrd@ornl.gov>
2014-05-11 14:52:07 -0400

    Clarify row and column iterators

--- lb_matrix.h
+++ lb_matrix.h
@@ -9,8 +9,8 @@
 	
 	#define lb_mat_element_ptr(A,i,j) (A.data + j * A.num_cols + i)
 	#define lb_mat_element(A,i,j) A.data[j * A.num_cols + i]
-	#define lb_mat_row_element(A,row,offset) A.data[offset + row * A.num_cols]
-	#define lb_mat_col_element(A,col,offset) A.data[col + offset * A.num_cols]
+	#define lb_mat_row_element(A,i,j) A.data[i * A.num_cols + j]
+	#define lb_mat_col_element(A,i,j) A.data[j * A.num_cols + i]
 	#define lb_vec_element_ptr(v,j) v.data + j;
 	void lbmm(Matrix A, Matrix B, Matrix result);
 	void lbma(Matrix A, Matrix B, Matrix result);
