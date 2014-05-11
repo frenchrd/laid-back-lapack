70db88157b8f12c955d296f4f406c21600ebbedf
Robert D. French <frenchrd@ornl.gov>
2014-05-11 12:19:27 -0400

    Make walltimer more convenient to use

--- lb_walltimer.h
+++ lb_walltimer.h
@@ -1,5 +1,10 @@
 #ifndef LB_WALLTIMER_INCLUDED
-	#define Initialize_Walltimer() double time0 = MPI_Wtime(); double time1;
-	#define Walltimer_Label(rank_id,msg) time1 = MPI_Wtime(); printf("## [Rank %d] " msg " (%fs)\n",rank_id,time1 - time0); time0 = time1;
+	typedef struct walltimer_metadata {
+		int rank_id;
+		double time0;
+		double time1;
+	} WalltimerMetadata;
+	#define Initialize_Walltimer(rank) WalltimerMetadata __wtmmd; __wtmmd.time0 = MPI_Wtime(); __wtmmd.rank_id = rank;
+	#define Walltimer_Label(msg) __wtmmd.time1 = MPI_Wtime(); printf("## [Rank %d] " msg " (%fs)\n",__wtmmd.rank_id,__wtmmd.time1 - __wtmmd.time0); __wtmmd.time0 = __wtmmd.time1;
 	#define LB_WALLTIMER_INCLUDED
 #endif
